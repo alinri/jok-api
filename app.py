b2820b659590834110888ef3ecdfc5be6d1d7fd3
Ali Noori <13noori79@gmail.com>
2022-07-06 21:06:48 +0430

    Init commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+from urllib import parse
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey, func
+from sqlalchemy.orm import relationship
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    f'mysql+mysqldb://{os.environ["FLASk_DB_USER"]}:'\
+    f'{parse.quote(os.environ["FLASk_DB_PASSWORD"])}@'\
+    f'{os.environ["FLASk_DB_HOST"]}/'\
+    f'{os.environ["FLASk_DB_NAME"]}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    post_id = Column(Integer, primary_key=True)
+    post_content = Column(String(length=2000))
+    four_joke_id = Column(String(length=32))
+    category_id = Column(Integer, ForeignKey('category.category_id'))
+    category = relationship('Category', back_populates='posts')
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    category_id = Column(Integer, primary_key=True)
+    category_name = Column(String(length=255))
+    category_type = Column(String(length=16))
+    posts = relationship('Post')
+
+
+class PostSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Post
+        include_fk = True
+
+    post_id = ma.auto_field()
+    post_content = ma.auto_field()
+    post_type = fields.String(attribute='category.category_type')
+    category_id = ma.auto_field()
+
+
+class CategorySchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Category
+        include_fk = True
+
+
+post_schema = PostSchema()
+posts_schema = PostSchema(many=True)
+category_schema = CategorySchema()
+categories_schema = CategorySchema(many=True)
+
+
+@app.route('/random/')
+def random_post():
+    post = Post.query.order_by(func.rand()).limit(1).first()
+
+    return post_schema.dump(post)
